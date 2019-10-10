c2df3e1edd46421e849acafa806c7b184f82e30c
Akash Karmakar <akashkarmakar787@gmail.com>
2019-10-09 22:24:42 -0700

    unit testing for hotel validation added

---
+++ test/unit/hotel_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+class PostTest < ActiveSupport::TestCase
+	
+	test 'should not save hotel without hotelname and address' do 
+		hotel=Hotel.new
+		assert_not hotel.save, 'title should not be nil'
+	end
+		
+end
